9f9d23f7c354993cb26fb6cd23500bf988ef7a6e
Md Fahimul Karim <f.nahian1997@gmail.com>
2025-08-25 10:40:30 +0600

    circle & cross mark updated

--- src/component/tictactoe/Square.tsx
+++ src/component/tictactoe/Square.tsx
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion";
+import { motion, type Variants } from "motion/react";
 
 interface SquareProps {
   ind?: number;
@@ -6,16 +6,60 @@ interface SquareProps {
   clsName?: string;
 }
 
+const draw: Variants = {
+  hidden: { pathLength: 0, opacity: 0 },
+  visible: (i: number) => {
+    const delay = i * 0.5;
+    return {
+      pathLength: 1,
+      opacity: 1,
+      transition: {
+        pathLength: { delay, type: "spring", duration: 1.5, bounce: 0 },
+        opacity: { delay, duration: 0.01 },
+      },
+    };
+  },
+};
+
 const Square: React.FC<SquareProps> = ({ ind, updateSquares, clsName }) => {
   const handleClick = () => {
-    if(ind === undefined) return;
+    if (ind === undefined) return;
     updateSquares?.(ind);
   };
   return (
-    <motion.div initial={{ scale: 0 }} animate={{ scale: 1 }} className="square" onClick={handleClick}>
-      {clsName && <motion.span initial={{ scale: 0 }} animate={{ scale: 1 }} className={clsName}></motion.span>}
+    <motion.div className="square" onClick={handleClick}>
+      {clsName === "x" && <PathDrawingCross />}
+      {clsName === "o" && <PathDrawingCircle />}
     </motion.div>
   );
 };
 
 export default Square;
+
+function PathDrawingCircle() {
+  return (
+    <motion.svg width="100%" height="100%" viewBox="0 0 100 100" initial="hidden" animate="visible" style={image}>
+      <motion.circle className="circle-path" cx="50" cy="50" r="35" stroke="#ffa02e" variants={draw} custom={0.5} style={shape} />
+    </motion.svg>
+  );
+}
+
+function PathDrawingCross() {
+  return (
+    <motion.svg width="100%" height="100%" viewBox="0 0 100 100" initial="hidden" animate="visible" style={image}>
+      <motion.line x1="25" y1="25" x2="75" y2="75" stroke="#ff0088" custom={0.5} variants={draw} style={shape} />
+      <motion.line x1="25" y1="75" x2="75" y2="25" stroke="#ff0088" custom={0.5} variants={draw} style={shape} />
+    </motion.svg>
+  );
+}
+
+const image: React.CSSProperties = {
+  width: "100%",
+  height: "100%",
+};
+
+const shape: React.CSSProperties = {
+  strokeWidth: 5,
+  strokeLinecap: "round",
+  fill: "transparent",
+};
